3322aaa6e838f4f9ed3f1eedf2639c93d08cb656
Meher90 <gharsallah.meher@gmail.com>
2020-09-27 20:49:31 +0200

    deuxieme commit

--- app/src/main/java/com/mg/gittest/MainActivity.kt
+++ app/src/main/java/com/mg/gittest/MainActivity.kt
@@ -7,6 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // premier commit 
+        // premier commit
+
+        // deuxième vague
     }
 }
