7510ce8960146ed70f3b5b6a4902f49f99368c35
Meher90 <gharsallah.meher@gmail.com>
2020-09-27 22:44:13 +0200

    Revert "back to a previous version"
    This reverts commit e0d7e59aebade3d0925f3ab91d8c6609a5813611.

--- app/src/main/java/com/mg/gittest/MainActivity.kt
+++ app/src/main/java/com/mg/gittest/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // premier commit
+
+        // deuxième vague
+
+        // 3eme commit 
     }
 }
